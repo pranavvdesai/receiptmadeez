1804a2b1bceb391fc0416c14d115b3a95ea31805
pranavvdesai <pranavdesai.psd@gmail.com>
2022-07-03 00:36:05 +0530

    feat: vertical cards

--- components/Card.js
+++ components/Card.js
@@ -1,22 +1,90 @@
-import React from 'react';
-import { VStack, Box, Divider } from 'native-base';
+import * as React from "react";
 
+import {
+  Text,
+  Link,
+  HStack,
+  Center,
+  Heading,
+  Switch,
+  useColorMode,
+  NativeBaseProvider,
+  extendTheme,
+  VStack,
+  Box,
+  Flex,
+  Spacer,
+  FormControl,
+  Input,
+  Button,
+  Image,
+  View,
+  AspectRatio,
+  Stack,
+} from "native-base";
 export default function () {
-    return (
-        <Box border="1" borderRadius="md">
-            <VStack space="4" divider={<Divider />}>
-                <Box px="4" pt="4">
-                    NativeBase
-                </Box>
-                <Box px="4">
-                    NativeBase is a free and open source framework that enable developers
-                    to build high-quality mobile apps using React Native iOS and Android
-                    apps with a fusion of ES6.
-                </Box>
-                <Box px="4" pb="4">
-                    GeekyAnts
-                </Box>
-            </VStack>
+  return (
+    <Box alignItems="center" >
+      <Box
+        maxW="80"
+        rounded="lg"
+        overflow="hidden"
+        borderColor="coolGray.200"
+        borderWidth="1"
+        _web={{
+          shadow: 2,
+          borderWidth: 0,
+        }}
+      >
+        <Box>
+          <AspectRatio w="100%" ratio={16 / 9}>
+            <Image
+              source={{
+                uri: "https://www.holidify.com/images/cmsuploads/compressed/Bangalore_citycover_20190613234056.jpg",
+              }}
+              alt="image"
+            />
+          </AspectRatio>
         </Box>
-    );
+        <Stack p="4" space={3}>
+          <Stack space={2}>
+            <Heading size="md" ml="-1">
+              The Garden City
+            </Heading>
+            <Text
+              fontSize="xs"
+              _light={{
+                color: "violet.500",
+              }}
+              _dark={{
+                color: "violet.400",
+              }}
+              fontWeight="500"
+              ml="-0.5"
+              mt="-1"
+            >
+              The Silicon Valley of India.
+            </Text>
+          </Stack>
+          <Text>
+            Bengaluru (also called Bangalore) is the center of India's high-tech
+            industry. The city is also known for its parks and nightlife.
+          </Text>
+          <HStack alignItems="center" space={4} justifyContent="space-between">
+            <HStack alignItems="center">
+              <Text
+                color="coolGray.600"
+                _dark={{
+                  color: "warmGray.200",
+                }}
+                fontWeight="400"
+              >
+                6 mins ago
+              </Text>
+            </HStack>
+          </HStack>
+        </Stack>
+      </Box>
+    </Box>
+  );
 }
