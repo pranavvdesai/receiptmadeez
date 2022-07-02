36a0c135b05ea4a3ff1feb69232f0390434ec7c2
pranavvdesai <pranavdesai.psd@gmail.com>
2022-07-03 00:36:31 +0530

    feat: home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,77 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
-
-
+import * as React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import { Platform } from "react-native";
+import ProfileScreen from "./ProfileScreen";
+import { SavedReceipt } from "./SavedReceipt";
+import {
+  Text,
+  Link,
+  HStack,
+  Center,
+  Heading,
+  Switch,
+  useColorMode,
+  NativeBaseProvider,
+  extendTheme,
+  VStack,
+  Box,
+  Flex,
+  Spacer,
+  FormControl,
+  Input,
+  Button,
+  Image,
+  View,
+  AspectRatio,
+  Stack,
+} from "native-base";
+const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+import Card from "../components/Card";
+import HCard from "../components/HCard";
 export function HomeScreen() {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
-        </View>
-    );
+  return (
+    <View
+      style={{
+        flex: 1,
+        overflowX: "hidden",
+      }}
+      direction="column"
+      bg="red.500"
+    >
+      <Box maxWidth="100%">
+        <HCard />
+      </Box>
+      <Box flex={1} flexDirection="row" mx="20" flexWrap="wrap" justifyContent="space-between" >
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </Box>
+
+      {/* <Flex
+          flex={1}
+          bg="yellow.500"
+          direction={["row", "column"]}
+          justifyContent="space-between"
+          m="10"
+          mx="20"
+          flexWrap="wrap"
+          _dark={{
+            borderColor: "primary",
+            backgroundColor: "success",
+          }}
+          _light={{
+            backgroundColor: "success",
+          }}
+        >
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </Flex> */}
+    </View>
+  );
 }
